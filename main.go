a057483527733c683136e9088d037feb46247c23
Vitalii Kudriavets <14039142+VitaliiKudriavets@users.noreply.github.com>
2024-03-07 21:02:08 +0200

    Improve performance by implementing a concurrency mechanism in main function

--- main.go
+++ main.go
@@ -7,12 +7,20 @@ import (
 
 func main() {
 	ch := make(chan int)
+	data := make(chan int)
+	exit := make(chan int)
 	go sayHello(ch)
-	fmt.Println(<-ch)
-	fmt.Println(<-ch)
-	fmt.Println(<-ch)
-	fmt.Println(<-ch)
-	fmt.Println(<-ch)
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-data)
+		}
+		exit <- 0
+	}()
+	selectOne(data, exit)
+
+	for i := range ch {
+		fmt.Println(i)
+	}
 
 	Say("The End")
 }
@@ -25,5 +33,19 @@ func sayHello(exit chan int) {
 	for i := 0; i < 5; i++ {
 		time.Sleep(100 * time.Millisecond)
 		exit <- i
+	}
+	close(exit)
+}
+
+func selectOne(data, exit chan int) {
+	x := 0
+	for {
+		select {
+		case data <- x:
+			x += 1
+		case <-exit:
+			fmt.Println("exit")
+			return
+		}
 	}
 }
