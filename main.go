21e58a6a126060b282b9f157dbe3cefce38601bf
Vitalii Kudriavets <14039142+VitaliiKudriavets@users.noreply.github.com>
2024-03-07 20:24:33 +0200

    Add main.go file

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	ch := make(chan int)
+	go sayHello(ch)
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+
+	Say("The End")
+}
+
+func Say(word string) {
+	fmt.Println(word)
+}
+
+func sayHello(exit chan int) {
+	for i := 0; i <= 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		exit <- i
+	}
 }
